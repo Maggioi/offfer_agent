457eb4281bc41177638ec4824660f36399d253a5
Maggioi <michalmagala00@gmail.com>
2026-05-14 16:42:13 +0200

    Reszta do roboty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import docx
 
 doc = docx.Document("krolik_doswiadczalny.docx")
-print(len(doc.paragraphs))
-print(doc.paragraphs[0].text)
+#(len(doc.paragraphs))
+
+print(len(doc.tables))
+
+for row in doc.tables[2].rows:
+    for cell in row.cells:
+        print(cell.text)
